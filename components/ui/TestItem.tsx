f190f04fc0cca0764f96ea916183714c60770690
Timmi0-o <tima53419@gmail.com>
2024-06-07 01:51:48 +0700

    Fixed ошибки TypeScript

--- components/ui/TestItem.tsx
+++ components/ui/TestItem.tsx
@@ -22,14 +22,7 @@ export const TestItem = ({
 		<>
 			<div className='relative flex flex-col items-center gap-[20px] lg-w-[350px] xl:w-[400px] h-[600px] border-[#ADB5BD] border-[1px] rounded-[12px] cursor-pointer duration-300 hover:shadow-lg '>
 				<div className='relative w-[300px] xl:w-[380px] h-[260px] rounded-[8px] bg-[#E9ECEF] border-[1px] border-[#ADB5BD] mt-[10px]'>
-					<Image
-						className='rounded-[8px]'
-						src={
-							'https://psy-files.ru/wp-content/uploads/b/1/f/b1f03092e33cd71e229ca532faad64c9.jpg'
-						}
-						fill
-						alt=''
-					/>
+					<Image className='rounded-[8px]' src={'/image1.jpg'} fill alt='' />
 				</div>
 				<div className='flex flex-col gap-[30px] lg:w-[300px] xl:w-[380px]'>
 					<p className='text-[22px] md:text-[28px] font-medium text-[#343A40] px-[5px] rounded-[8px] bg-[#DEE2E6] border-x-[1px] border-x-[#212529] min-w-[300px] text-center'>
